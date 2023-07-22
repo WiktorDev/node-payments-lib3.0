ff8a0dbf34707de7fce803f470ad95a5def868b2
Wiktor <mcserw4@wp.pl>
2023-07-22 14:15:10 +0200

    fixed icehost payments

--- src/payments/wallets/icehost/icehost.payment.ts
+++ src/payments/wallets/icehost/icehost.payment.ts
@@ -3,6 +3,7 @@ import {PaymentGeneratedEntity} from "../../../entities/paymentGenerated.entity"
 import {IcehostParams} from "./icehost.params";
 import {validateObject} from "../../../utils/validate.function";
 import {PaymentException} from "../../../exceptions/payment.exception";
+import {isJson} from "../../../utils/isJson.function";
 
 export class IcehostPayment extends BasePayment {
   private apikey: string;
@@ -15,13 +16,16 @@ export class IcehostPayment extends BasePayment {
   }
 
   async generatePayment(): Promise<PaymentGeneratedEntity> {
-	await validateObject(IcehostParams, this.params)
-	const response = await this.doRequest('https://dash.icehost.pl/api/client/wallet/up', "POST", this.params, {
-	  'Authorization': `Bearer ${this.apikey}`,
-	  'Content-Type': 'application/json'
-	})
-	if (response.status !== 200)
-	  throw new PaymentException(`[IceHost] ${response.data.errors[0].code}`)
-	return new PaymentGeneratedEntity(response.data.url, response.data.id);
+		await validateObject(IcehostParams, this.params)
+		const response = await this.doRequest('https://dash.icehost.pl/api/client/wallet/up', "POST", this.params, {
+			'Authorization': `Bearer ${this.apikey}`,
+			'Content-Type': 'application/json'
+		})
+		if (response.status !== 200) {
+			if (!isJson(response.data))
+				throw new PaymentException(`[IceHost] Serwis zostal zablokowany przez IceHost!`)
+			throw new PaymentException(`[IceHost] ${response.data.errors[0].code}`)
+		}
+		return new PaymentGeneratedEntity(response.data.url, response.data.id);
   }
 }
